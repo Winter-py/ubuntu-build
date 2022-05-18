941d8d0d8367968945f6b43ed2e8a612c824822b
Steven Owatemi <isosteven@gmail.com>
2022-05-18 11:41:37 +0100

    added vscode

--- setup.sh
+++ setup.sh
@@ -4,6 +4,11 @@ sudo apt-get upgrade
 #Development Stuff
 sudo apt install git -y
 sudo apt-get install python -y
+sudo apt install software-properties-common apt-transport-https wget -y
+wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
+sudo apt install code
+
 
 #Network Tools 
 sudo apt-get install net-tools -y
