7f407445da5a824c8e36fc806fba93fde377113b
Steven Owatemi <isosteven@gmail.com>
2022-05-19 13:50:23 +0100

    dir Fix

--- init.sh
+++ init.sh
@@ -3,10 +3,7 @@
 #git install
 sudo apt-get install git -y
 
-#GitHub stuff
-repository= https://github.com/Winter-py/ubuntu-build.git
-
-directory="Documents"
+directory = ~/Documents 
 
 # Set the path to the directory where the script is located
 
@@ -14,7 +11,7 @@ cd "~/$directory"
 sudo mkdir Build 
 cd Build
 
-git clone "$repository" 
+git clone https://github.com/Winter-py/ubuntu-build.git 
 
 chmod +x ./ubuntu-build/setup.sh
 sudo ./ubuntu-build/setup.sh
