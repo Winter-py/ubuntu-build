567db3eeb4f64dab58d83a68d88065f97bc9d9be
Steven Owatemi <isosteven@gmail.com>
2022-05-19 14:01:34 +0100

    removed directory element

--- init.sh
+++ init.sh
@@ -3,11 +3,8 @@
 #git install
 sudo apt-get install git -y
 
-directory = ~/Documents 
-
 # Set the path to the directory where the script is located
 
-cd "~/$directory"
 sudo mkdir Build 
 cd Build
 
--- setup.sh
+++ setup.sh
@@ -5,9 +5,9 @@ sudo apt-get upgrade
 sudo apt install git -y
 sudo apt-get install python -y
 sudo apt install software-properties-common apt-transport-https wget -y
-wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
-sudo apt install code
+wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add - 
+sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" 
+sudo apt install code -y
 
 
 #Network Tools 
@@ -17,13 +17,13 @@ sudo apt-get install nmap -y
 
 #Install brave browser 
 
-sudo apt install apt-transport-https curl
+sudo apt install apt-transport-https curl -y    
 
-sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg 
 
 echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
 
-sudo apt update
+sudo apt update 
 
 sudo apt install brave-browser -y
 
